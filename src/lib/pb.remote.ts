e2ecbac942362c48e2a7ec3e3ab966888ed71bbf
Krist Ponpairin <krist7599555@gmail.com>
2026-01-03 04:40:40 +0700

    chore: pocketbase remove file url

--- src/lib/pb.remote.ts
+++ src/lib/pb.remote.ts
@@ -1,6 +1,5 @@
 import { query } from '$app/server';
 import { pb } from './pocketbase';
-import * as v from 'valibot';
 import type { PostsResponse, TagsRecord } from './pocketbase/pb_types';
 
 export const getPosts = query(async () => {
@@ -11,13 +10,3 @@ export const getPosts = query(async () => {
             expand: 'tags'
         });
 });
-export const getFileUrl = query(
-    v.object({
-        collectionName: v.string(),
-        id: v.string(),
-        filename: v.string()
-    }),
-    ({ collectionName, id, filename }) => {
-        return pb().files.getURL({ collectionName, id }, filename);
-    }
-);
